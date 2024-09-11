e69c2db7d59e301a5b868f789e8e8e1175367903
Rohit2001K <rohitkumavatgame@gmail.com>
2024-09-11 16:20:34 +0530

    Model creation and Profile rendering
    Model creation and showing all users in home page

--- Base/users/models.py
+++ Base/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=20,null=False)
+    email=models.EmailField(unique=True)
+    bio=models.TextField(max_length=500)
+    id=models.UUIDField(default=uuid.uuid1,primary_key=True)
+    pic=models.ImageField(default='user.jpg')
+    
+    def __str__(self):
+        return str(self.name)
--- Base/users/views.py
+++ Base/users/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+
+
+#home page view
 def home(request):
-    return render(request,'users/home.html')
+    data=Profile.objects.all()
+    content={
+        'data':data
+    }
+    return render(request,'users/home.html',content)
